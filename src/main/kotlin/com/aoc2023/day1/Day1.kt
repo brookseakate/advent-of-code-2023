eb84cb77509433ea4c616ddab5e529eb39b745bf
Kate Shaffer <kate.shaffer@gmail.com>
2023-12-08 16:53:47 -0800

    WIP Day 1, part 2 – not quite working

--- src/main/kotlin/com/aoc2023/day1/Day1.kt
+++ src/main/kotlin/com/aoc2023/day1/Day1.kt
@@ -26,9 +26,15 @@ class Day1 {
     }
 
     private fun getCalibrationValue(string: String): Int {
+
       val regex = Regex("\\d")
 
-      val digits = regex.findAll(string).map { it.value }.toList()
+      val digits = regex
+        .findAll(
+          replaceDigitStringsWithDigits(string)
+        )
+        .map { it.value }
+        .toList()
 
       // TODO remove logs
       println(digits)
@@ -36,6 +42,45 @@ class Day1 {
       println("Result: $result")
 
       return result
+    }
+
+    private fun replaceDigitStringsWithDigits(string: String): String {
+      println("Undigitized: $string")
+
+      val regex = Regex(
+        Digit.values()
+          .joinToString("|") { "(" + it.string + ")" }
+      )
+
+      val digitized = string.replace(
+          regex = regex,
+          transform = { it.value.toDigit().integer.toString() }
+        )
+
+      // TODO remove logs
+      println("Digitized: $digitized")
+      return digitized
+    }
+
+    enum class Digit(
+      val string: String,
+      val integer: Int,
+    ) {
+      ZERO("zero", 0),
+      ONE("one", 1),
+      TWO("two", 2),
+      THREE("three", 3),
+      FOUR("four", 4),
+      FIVE("five", 5),
+      SIX("six", 6),
+      SEVEN("seven", 7),
+      EIGHT("eight", 8),
+      NINE("nine", 9),
+      ;
+
+    }
+    private fun String.toDigit(): Digit {
+      return Digit.valueOf(this.uppercase())
     }
   }
 }
